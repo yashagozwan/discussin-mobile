0283a7cb2398a19aab52f097adc0489fda6c686d
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-11-17 19:06:27 +0700

    Sign In Screen

--- lib/src/screen/sign_in/sign_in_screen.dart
+++ lib/src/screen/sign_in/sign_in_screen.dart
@@ -24,77 +24,119 @@ class _SignInScreenState extends ConsumerState<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset('assets/images/group.png', scale: 3),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(13),
-                    isDense: true,
-                    hintText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    contentPadding: EdgeInsets.all(13),
-                    isDense: true,
-                    hintText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                ElevatedButtonPro(
-                  onPressed: () {},
-                  title: 'Login',
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Stack(
-                  children: [
-                    const Divider(
-                      thickness: 2,
+      body: SingleChildScrollView(
+        child: Form(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Image.asset(
+                      'assets/images/Discussin_Login_Image.png',
+                      scale: 1,
                     ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 30,
-                        color: Colors.white,
-                        child: const Text('Or'),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: Text.rich(
-                    TextSpan(text: "Don't have account?", children: [
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text.rich(
                       TextSpan(
-                        text: " Sign Up",
+                        text: "Alloha!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 32,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.all(13),
+                      prefixIcon: Icon(Icons.email_outlined),
+                      isDense: true,
+                      hintText: 'Email ID',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      fillColor: Colors.amber,
+                      contentPadding: EdgeInsets.all(13),
+                      prefixIcon: Icon(Icons.lock_outline_rounded),
+                      isDense: true,
+                      hintText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Forget Password?",
                         recognizer: TapGestureRecognizer()..onTap = () {},
                         style: const TextStyle(fontWeight: FontWeight.w600),
                       ),
-                    ]),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButtonPro(
+                    onPressed: () {},
+                    title: 'Login',
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // Stack(
+                  //   children: [
+                  //     const Divider(
+                  //       thickness: 2,
+                  //     ),
+                  //     Align(
+                  //       alignment: Alignment.center,
+                  //       child: Container(
+                  //         alignment: Alignment.center,
+                  //         width: 30,
+                  //         color: Colors.white,
+                  //         child: const Text('Or'),
+                  //       ),
+                  //     )
+                  //   ],
+                  // ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text.rich(
+                      TextSpan(text: "New to Discuss.in?", children: [
+                        TextSpan(
+                          text: " Register",
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                          style: const TextStyle(fontWeight: FontWeight.w600),
+                        ),
+                      ]),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
