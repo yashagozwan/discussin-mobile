739b1b2b5b3e95ac2387937dcd92a34a48e7b3c7
prkrx <richowardiyanto1@gmail.com>
2022-11-23 18:54:47 +0700

    add profile screen

--- lib/src/screen/profile/profile_screen.dart
+++ lib/src/screen/profile/profile_screen.dart
@@ -1,5 +1,9 @@
+import 'package:discussin_mobile/src/util/colors.dart';
+import 'package:discussin_mobile/src/widget/text_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
 
 class ProfileScreen extends ConsumerStatefulWidget {
   const ProfileScreen({super.key});
@@ -11,9 +15,22 @@ class ProfileScreen extends ConsumerStatefulWidget {
 class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(children: const [
+              TextPro(
+              'Profile',
+              color: primaryBlue,
+              fontWeight: FontWeight.w600,
+            ),
+            SizedBox(width: 280),
+            Icon(Icons.notifications_sharp,
+            
+            )
+          ],
+        ),
+        backgroundColor: yellow,
+        elevation: 0,
       ),
     );
   }
