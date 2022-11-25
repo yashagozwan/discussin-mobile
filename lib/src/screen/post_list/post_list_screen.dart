3b68d54d7ca4c161f92e4298d55c44abb6dbd4c0
khokhonicho@gmail.com <111693104+VelocyraptorZ@users.noreply.github.com>
2022-11-26 01:01:15 +0700

    add feature Post Detail Screen

--- lib/src/screen/post_list/post_list_screen.dart
+++ lib/src/screen/post_list/post_list_screen.dart
@@ -7,6 +7,8 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../post_detail/post_detail_screen.dart';
+
 class PostListScreen extends ConsumerStatefulWidget {
   final int selectedTopic;
   const PostListScreen({Key? key, this.selectedTopic = 1}) : super(key: key);
@@ -212,7 +214,7 @@ class _PostListScreenState extends ConsumerState<PostListScreen> {
                         alignment: Alignment.centerLeft,
                         child: Text(
                           post.name,
-                          textAlign: TextAlign.left,
+                          textAlign: TextAlign.justify,
                           style: const TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.w600,
@@ -224,9 +226,14 @@ class _PostListScreenState extends ConsumerState<PostListScreen> {
                       ),
                       Align(
                         alignment: Alignment.centerLeft,
-                        child: Text(
-                          post.description,
-                          textAlign: TextAlign.left,
+                        child: SizedBox(
+                          height: 100,
+                          child: Text(
+                            post.description,
+                            textAlign: TextAlign.justify,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 5,
+                          ),
                         ),
                       ),
                     ],
@@ -256,14 +263,33 @@ class _PostListScreenState extends ConsumerState<PostListScreen> {
                           const Text('10'),
                           Padding(
                             padding: const EdgeInsets.only(left: 85.0),
-                            child: Text.rich(
-                              TextSpan(
-                                text: "Read More",
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {},
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.w600,
-                                  color: primaryBlue,
+                            child: InkWell(
+                              customBorder: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              splashColor: Colors.black12,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return PostDetailScreen(post: post);
+                                    },
+                                  ),
+                                );
+                              },
+                              child: const SizedBox(
+                                width: 100,
+                                child: Center(
+                                  child: Text.rich(
+                                    TextSpan(
+                                      text: "Read More",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        color: primaryBlue,
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ),
                             ),
--- lib/src/service/post_service.dart
+++ lib/src/service/post_service.dart
@@ -8,7 +8,8 @@ class PostService {
         id: 10,
         name: 'Do you have unforgettable climbing experience?',
         description:
-            'It all started back in 2008. I was in my second year of university, and one night my friend Megan decided that she wanted to go to a lecture about mountains as a ploy to meet hot guys, and she dragged me alone',
+            'It all started back in 2008. I was in my second year of university, and one night my friend Megan decided that she wanted to go to a lecture about mountains as a ploy to meet hot guys, and she dragged me alone, It all started back in 2008. I was in my second year of university, and one night my friend Megan decided that she wanted to go to a lecture about mountains as a ploy to meet hot guys, and she dragged me alonoe. To see Everest, one must go to a lookout called Tiger Hill, thirteen miles to the southeast.',
+        imageUrl: 'https://wallpaperaccess.com/full/223237.jpg',
         userId: 12,
         topicId: 1,
         createdAt: DateTime.now().millisecondsSinceEpoch,
@@ -16,9 +17,11 @@ class PostService {
       ),
       Post(
         id: 11,
-        name: 'Do you have unforgettable climbing experience?',
+        name: 'Did you know about Breakdance Lizard?',
         description:
-            'It all started back in 2008. I was in my second year of university, and one night my friend Megan decided that she wanted to go to a lecture about mountains as a ploy to meet hot guys, and she dragged me alone',
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        imageUrl:
+            'https://images.pling.com/img/00/00/61/26/90/1536184/6337a702d0cd417543a99905b75ea7f0ce0332a8e2bd53fae2468b8e0ca92826bb0e.jpg',
         userId: 12,
         topicId: 2,
         createdAt: DateTime.now().millisecondsSinceEpoch,
@@ -26,9 +29,11 @@ class PostService {
       ),
       Post(
         id: 12,
-        name: 'Do you have unforgettable climbing experience?',
+        name: 'Talk about Flutter',
         description:
-            'It all started back in 2008. I was in my second year of university, and one night my friend Megan decided that she wanted to go to a lecture about mountains as a ploy to meet hot guys, and she dragged me alone',
+            'Flutter is Google portable UI toolkit for crafting beautiful, natively compiled applications for mobile, web, and desktop from a single codebase. Flutter works with existing code, is used by developers and organizations around the world, and is free and open source.',
+        imageUrl:
+            'https://www.kibrispdr.org/data/748/nature-wallpaper-hd-1080p-27.jpg',
         userId: 12,
         topicId: 3,
         createdAt: DateTime.now().millisecondsSinceEpoch,
