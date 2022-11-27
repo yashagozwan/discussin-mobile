94931d66230f8b25657a17055cd7b8ca9853f063
prkrx <richowardiyanto1@gmail.com>
2022-11-27 23:01:02 +0700

    completed profile screen

--- lib/src/screen/profile/profile_screen.dart
+++ lib/src/screen/profile/profile_screen.dart
@@ -41,35 +41,171 @@ class _ProfileScreenState extends ConsumerState<ProfileScreen> {
 }
 
 Widget editprofile() {
-  return Column(children: [
-          Stack(
+  return Column(
+    children: [
+          Row(
             children: [
-              Container(
-                  padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
-                  child: CachedNetworkImage(
-                  imageUrl: 'https://picsum.photos/250?image=9',
-                  alignment: Alignment.center,
-                  imageBuilder: (context, image) => CircleAvatar(
-                    backgroundImage: image,
-                    radius: 60,
+              Stack(
+                children: [
+                  Container(
+                      padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+                      child: CachedNetworkImage(
+                      imageUrl: 'https://picsum.photos/250?image=9',
+                      alignment: Alignment.center,
+                      imageBuilder: (context, image) => CircleAvatar(
+                        backgroundImage: image,
+                        radius: 60,
+                      ),
+                    ),
                   ),
-                ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(110, 110, 0, 0),
+                    width: 45,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      shape: BoxShape.circle,
+                  ),
+                  child: IconButton(
+                      iconSize: 22,
+                      icon: Icon(Icons.edit),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
               ),
               Container(
-                margin: EdgeInsets.fromLTRB(110, 110, 0, 0),
-                width: 45,
-                height: 35,
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  shape: BoxShape.circle,
-              ),
-              child: IconButton(
-                  iconSize: 22,
-                  icon: Icon(Icons.edit),
-                  onPressed: () {},
+                margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Jhon Legend",
+                    style: TextStyle(fontWeight: FontWeight.bold,
+                    fontSize: 18),
+                    ),
+                    Text("Student Collage"),
+                    Row(
+                      children: [
+                        Text("Followers : 25"),
+                        SizedBox(width: 15),
+                        Text("Following : 25")
+                      ],
+                    ),
+                    Text("Joined Since 2018")
+
+                  ],
                 ),
               )
             ],
-          )
+          ),
+          SizedBox(height: 15,),
+          Container(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: 360,
+              child: ElevatedButton(onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: StadiumBorder()
+                ),
+              child: Text("edit Profile")
+            ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 30, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.post_add,
+                      size: 30,
+                    ),
+                    SizedBox(width: 20,),
+                      Text("Discuss created",
+                      style: TextStyle(
+                      fontSize: 15),
+                      ),
+                    ],
+              ),
+          ),
+            SizedBox(height: 15,),
+          Padding(
+                    padding:EdgeInsets.symmetric(horizontal:10.0),
+                    child:Container(
+                    height:0.5,
+                    width:360,
+                    color:Colors.black),
+                    ),
+        Container(
+            margin: EdgeInsets.fromLTRB(30, 15, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.comment_bank_outlined,
+                      size: 30,
+                    ),
+                    SizedBox(width: 20,),
+                      Text("Discuss that you joined",
+                      style: TextStyle(
+                      fontSize: 15),
+                      ),
+                    ],
+              ),
+          ),
+            SizedBox(height: 15,),
+          Padding(
+                    padding:EdgeInsets.symmetric(horizontal:10.0),
+                    child:Container(
+                    height:0.6,
+                    width:360,
+                    color:Colors.black),
+                    ),
+        Container(
+            margin: EdgeInsets.fromLTRB(30, 15, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.edit_note,
+                      size: 30,
+                    ),
+                    SizedBox(width: 20,),
+                      Text("Draft",
+                      style: TextStyle(
+                      fontSize: 15),
+                      ),
+                    ],
+              ),
+          ),
+            SizedBox(height: 15,),
+          Padding(
+                    padding:EdgeInsets.symmetric(horizontal:10.0),
+                    child:Container(
+                    height:0.3,
+                    width:360,
+                    color:Colors.black),
+                    ),
+          Container(
+            margin: EdgeInsets.fromLTRB(30, 15, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.settings_outlined,
+                      size: 30,
+                    ),
+                    SizedBox(width: 20,),
+                      Text("Setting",
+                      style: TextStyle(
+                      fontSize: 15),
+                      ),
+                    ],
+              ),
+          ),
+            SizedBox(height: 15,),
+          Padding(
+                    padding:EdgeInsets.symmetric(horizontal:10.0),
+                    child:Container(
+                    height:0.3,
+                    width:360,
+                    color:Colors.black),
+                    ),
         ]);
 }
